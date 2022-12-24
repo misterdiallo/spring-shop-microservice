215c02a0e15572a22a4a8943e2d2d7c0b6092156
Mister Diallo <77695206+misterdiallo@users.noreply.github.com>
2022-12-25 05:28:56 +0800

    All eureka client error

--- product-service/src/main/java/com/misterdiallo/backend/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/misterdiallo/backend/productservice/ProductServiceApplication.java
@@ -1,9 +1,11 @@
 package com.misterdiallo.backend.productservice;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
